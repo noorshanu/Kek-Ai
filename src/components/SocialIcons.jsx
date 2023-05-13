3ae8cfff645c480777fce8b4e730cae7ba5b5788
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-05-13 14:24:55 +0500

    removed blacnk

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -5,19 +5,19 @@ import { FaReddit, FaTelegramPlane, FaTwitter } from "react-icons/fa";
 function SocialIcons() {
   return (
     <>
-      <a href="/" target="_blank" className="text-[1em] text-primary">
+      <a href="/" className="text-[1em] text-primary">
         <SiGitbook />
       </a>
-      <a href="/" target="_blank" className="text-[1em] text-primary">
+      <a href="/" className="text-[1em] text-primary">
         <FaReddit />
       </a>
-      <a href="/" target="_blank" className="text-[1em] text-primary">
+      <a href="/" className="text-[1em] text-primary">
         <FaTelegramPlane />
       </a>
-      <a href="/" target="_blank" className="text-[1em] text-primary">
+      <a href="/" className="text-[1em] text-primary">
         <FaTwitter />
       </a>
-      <a href="/" target="_blank" className="text-[1em] text-primary">
+      <a href="/" className="text-[1em] text-primary">
         <BsMedium />
       </a>
     </>
