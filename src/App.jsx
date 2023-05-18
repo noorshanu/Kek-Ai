1b7542344ef219166fd6fda32d545da9968841cf
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2023-05-18 12:15:03 +0500

    changed loader position

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,22 @@ function App() {
     }, 3000);
   }, []);
 
-  if (loading) {
-    return (
-      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-        {/* <img src="images/loader.gif" alt="" /> */}
-        <img
-          src="images/loader.gif"
-          className="max-w-[20rem] w-[90%] pointer-events-none select-none relative bottom-10"
-          alt="loader"
-        />
-      </div>
-    );
-  }
-
   return (
-    <div>
+    <div
+      className={`${
+        loading ? "h-screen overflow-y-hidden" : "h-auto overflow-y-auto"
+      }`}
+    >
+      {loading && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-[10000] bg-bg">
+          <img
+            src="images/loader.gif"
+            className="max-w-[20rem] w-[90%] pointer-events-none select-none relative bottom-10"
+            alt="loader"
+          />
+        </div>
+      )}
+
       <HomePage />
     </div>
   );
